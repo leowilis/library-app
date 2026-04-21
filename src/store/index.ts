2d758e9af4fba29f64d44d7055194de3a2e56489
Leo wilis <leowilis9898@gmail.com>
2026-04-22 01:40:12 +0700

    chore(store): cleanup unused cart state logic

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '@/store/authSlice'
 import uiReducer from '@/store/uiSlice'
-import cartReducer from '@/store/cartSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     ui: uiReducer,
-    cart: cartReducer,
   },
 })
 
