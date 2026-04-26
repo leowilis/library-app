90ea1eb10cd246e011bb5fc7e741c64173884d37
Leo wilis <leowilis9898@gmail.com>
2026-04-26 19:13:08 +0700

    chore(Layout): change px to px 3

--- src/components/layout/user/Layout.tsx
+++ src/components/layout/user/Layout.tsx
@@ -5,7 +5,7 @@ export default function UserLayout({ children }: { children: React.ReactNode }) 
   return (
     <div className="min-h-screen flex flex-col" style={{ backgroundColor: 'var(--primary-100)' }}>
       <Navbar />
-      <main className="px-4 pt-5 md:px-26 md:grow">
+      <main className="px-3 pt-5 md:px-26 md:grow">
         {children}
       </main>
       <Footer />
