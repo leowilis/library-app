f8d0fcf503115c8542cfa00318f401f3eef4f4aa
Leo wilis <leowilis9898@gmail.com>
2026-07-10 01:43:49 +0700

    chore(main): refactor QueryClientProvider to consume centralized queryClient from lib module

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,23 @@
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { store } from "./store/index.ts";
-import React from "react";
-import "./index.css";
-import App from "./App.tsx";
-import ReactDOM from "react-dom/client";
-import { Toaster } from 'sonner'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from 'sonner';
 
-const queryClient = new QueryClient();
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+import App from './App';
+import { store } from './store';
+import { queryClient } from '@/lib/react-query';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <App />
-          <Toaster richColors position="top-right" />
+          <Toaster richColors position='top-right' />
         </BrowserRouter>
       </QueryClientProvider>
     </Provider>
