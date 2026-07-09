5e27585d8a405d591c64a40036079adc33433cbf
Leo wilis <leowilis9898@gmail.com>
2026-07-09 18:24:17 +0700

    feat: add htmlFor on label tag

--- src/pages/admin/BookForm/FormField.tsx
+++ src/pages/admin/BookForm/FormField.tsx
@@ -1,17 +1,19 @@
 interface FormFieldProps {
+  id: string;
   label: string;
   required?: boolean;
   children: React.ReactNode;
 }
 
 export default function FormField({
+  id,
   label,
   required,
   children,
 }: FormFieldProps) {
   return (
     <div className='space-y-2'>
-      <label className='text-sm font-bold text-neutral-950'>
+      <label htmlFor={id} className='text-sm font-bold text-neutral-950'>
         {label}
         {required && <span className='ml-1 text-red-500'>*</span>}
       </label>
