e315d85ab7430895a43b167951556dd40e6961f4
Leo wilis <leowilis9898@gmail.com>
2026-07-10 00:07:25 +0700

    chore(home): create barrel export index file for home components organization

---
+++ src/pages/user/Home/index.ts
@@ -0,0 +1,3 @@
+export { default as CategorySection } from './CategorySection';
+export { default as RecommendationSection } from './RecommendationSection';
+export { default as PopularAuthorSection } from './PopularAuthorSection';
