71c05270979367ac9b45083543022230f0d56d02
Leo wilis <leowilis9898@gmail.com>
2026-07-10 00:33:42 +0700

    chore: remove old book detail components, wrong into folder

--- src/pages/admin/BookDetail/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as BookBreadcrumb } from './BookBreadcrumb';
-export { default as BookCover } from './BookCover';
-export { default as BookHeader } from './BookHeader';
-export { default as BookStats } from './BookStats';
-export { default as BookDescription } from './BookDescription';
-export { default as BookReviewSection } from './BookReviewSection';
-export { default as RelatedBooksSection } from './RelatedBooksSection';
-export { default as MobileBorrowBar } from './MobileBorrowBar';
