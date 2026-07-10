a3a37aad09cc6243d220a85fe13b9fa1046a0d66
Leo wilis <leowilis9898@gmail.com>
2026-07-11 02:18:15 +0700

    chore(admin): finalize local constants file for admin dashboard navigation array

---
+++ src/components/layout/admin/constants.ts
@@ -0,0 +1,20 @@
+import { ROUTES } from '@/constants';
+
+export const ADMIN_TABS = [
+  {
+    label: 'Dashboard',
+    path: ROUTES.AdminDashboard,
+  },
+  {
+    label: 'Borrowed List',
+    path: ROUTES.AdminBorrowed,
+  },
+  {
+    label: 'User',
+    path: ROUTES.AdminUsers,
+  },
+  {
+    label: 'Book List',
+    path: ROUTES.AdminBooks,
+  },
+];
