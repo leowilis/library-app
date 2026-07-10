b7fb084cf1ef8a68a3ebb39dbb51d059059756e0
Leo wilis <leowilis9898@gmail.com>
2026-07-11 02:19:54 +0700

    chore(admin): create central index barrel export for admin layout sub-modules

---
+++ src/components/layout/admin/index.ts
@@ -0,0 +1,4 @@
+export { default as AdminHeader } from './AdminHeader';
+export { default as AdminTabs } from './AdminTabs';
+export { default as ProfileDropdown } from './ProfileDropdown';
+export { default as LogoButton } from './LogoButton';
