48302cf7004b492b468b42703fd23451f7ac42fb
Leo wilis <leowilis9898@gmail.com>
2026-07-11 01:30:17 +0700

    chore(navigation): create central index barrel export for all navbar atoms and molecules

---
+++ src/components/layout/user/navbar/index.ts
@@ -0,0 +1,8 @@
+export { default as Navbar } from './Navbar';
+export { default as NavbarLogo } from './NavbarLogo';
+export { default as SearchBar } from './SearchBar';
+export { default as MobileSearch } from './MobileSearch';
+export { default as GuestMenu } from './GuestMenu';
+export { default as GuestDropdown } from './GuestDropdown';
+export { default as UserMenu } from './UserMenu';
+export { default as UserDropdown } from './UserDropdown';
