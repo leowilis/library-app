75455f654070c0a42054f781bb5e925a0f3061ef
Leo wilis <leowilis9898@gmail.com>
2026-07-12 02:38:15 +0700

    refactor(router): unify application architecture under a centralized AppRoutes orchestrator

--- src/app/routers/index.tsx
+++ src/app/routers/index.tsx
@@ -1,17 +1,17 @@
-import { Routes, Route } from 'react-router-dom'
-import { ROUTES } from '@/constants'
-import RegisterPage from '@/pages/auth/RegisterPage'
-import UserLogin from '@/pages/auth/LoginPage'
-import UserRouter from '@/app/routers/UserRouter'
-import AdminRouter from './AdminRouter'
+import { Route, Routes } from 'react-router-dom';
+import { ROUTES } from '@/constants';
+import RegisterPage from '@/pages/auth/Register/RegisterPage';
+import UserLogin from '@/pages/auth/LoginPage';
+import UserRouter from '@/app/routers/UserRouter';
+import AdminRouter from '@/app/routers/AdminRouter';
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path={ROUTES.Login} element={<UserLogin />} />
       <Route path={ROUTES.Register} element={<RegisterPage />} />
-      <Route path="/admin/*" element={<AdminRouter />} />
-      <Route path="/*" element={<UserRouter />} />
+      <Route path={`${ROUTES.AdminRoot}/*`} element={<AdminRouter />} />
+      <Route path='/*' element={<UserRouter />} />
     </Routes>
-  )
+  );
 }
