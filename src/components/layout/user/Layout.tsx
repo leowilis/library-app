0f40658988a3d884fe35126795b5d3e98dc25ec0
Leo wilis <leowilis9898@gmail.com>
2026-07-12 02:39:31 +0700

    fix(Layout): fix address from Navbar

--- src/components/layout/user/Layout.tsx
+++ src/components/layout/user/Layout.tsx
@@ -1,14 +1,19 @@
-import Footer from '@/components/layout/user/Footer'
-import Navbar from './Navbar'
+import Footer from '@/components/layout/user/Footer';
+import Navbar from './navbar/Navbar';
 
-export default function UserLayout({ children }: { children: React.ReactNode }) {
+export default function UserLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div className="min-h-screen flex flex-col" style={{ backgroundColor: 'var(--primary-100)' }}>
+    <div
+      className='min-h-screen flex flex-col'
+      style={{ backgroundColor: 'var(--primary-100)' }}
+    >
       <Navbar />
-      <main className="px-3 pt-5 md:px-26 md:grow">
-        {children}
-      </main>
+      <main className='px-3 pt-5 md:px-26 md:grow'>{children}</main>
       <Footer />
     </div>
-  )
+  );
 }
