17164ba9c17bf5a4018f056bff71a0c44e4de7c5
Leo wilis <leowilis9898@gmail.com>
2026-07-12 21:14:11 +0700

    chore: remove path from GuestDropdown and UserDropdown

--- src/components/layout/user/navbar/index.ts
+++ src/components/layout/user/navbar/index.ts
@@ -3,6 +3,4 @@ export { default as NavbarLogo } from './NavbarLogo';
 export { default as SearchBar } from './SearchBar';
 export { default as MobileSearch } from './MobileSearch';
 export { default as GuestMenu } from './GuestMenu';
-export { default as GuestDropdown } from './GuestDropdown';
 export { default as UserMenu } from './UserMenu';
-export { default as UserDropdown } from './UserDropdown';
