0c6da91e550b3192c5cedb8407636291a10794e4
Leo wilis <leowilis9898@gmail.com>
2026-07-13 01:54:42 +0700

    chore(store): remove unused uiReducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '@/store/authSlice'
-import uiReducer from '@/store/uiSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    ui: uiReducer,
   },
 })
 
