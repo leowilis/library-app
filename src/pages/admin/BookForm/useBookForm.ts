92b619eb9c09502132a013689dbfb7b731ff2e93
Leo wilis <leowilis9898@gmail.com>
2026-07-13 01:38:07 +0700

    refactor: update useEffect and setForm

--- src/pages/admin/BookForm/useBookForm.ts
+++ src/pages/admin/BookForm/useBookForm.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useNavigate, useParams } from 'react-router-dom';
 import { toast } from 'sonner';
