51d11d0ae23d4132ef1415e5dcba85027079194f
Leo wilis <leowilis9898@gmail.com>
2026-07-12 10:14:16 +0700

    style(admin): update bg using primary

--- src/pages/admin/BookForm/BookSubmitButton.tsx
+++ src/pages/admin/BookForm/BookSubmitButton.tsx
@@ -11,7 +11,7 @@ export default function BookSubmitButton({
     <button
       type='submit'
       disabled={loading}
-      className='w-full rounded-full bg-[#1c65da] py-3.5 text-sm font-semibold text-white transition-colors hover:bg-blue-700 disabled:opacity-60'
+      className='w-full rounded-full bg-primary-300 py-3.5 text-sm font-semibold text-white transition-colors hover:bg-blue-700 disabled:opacity-60'
     >
       {loading ? 'Saving...' : isEdit ? 'Update Book' : 'Add Book'}
     </button>
